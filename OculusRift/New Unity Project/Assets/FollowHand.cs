8ecfec5cf04c7fad562c5706b6d1a5d070106ca5
Dyer,Richard R.J <r.dyer@student.fontys.nl>
2017-03-16 11:51:22 +0100

    InitialUpload

---
+++ OculusRift/New Unity Project/Assets/FollowHand.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowHand : MonoBehaviour {
+
+    public GameObject player;
+    public float height;
+    public float distance; 
+    private Vector3 offset;
+    private Transform trans;
+	void Start () {
+        offset = transform.position - player.transform.position;
+        trans = transform;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        trans.position = new Vector3(player.transform.position.x, player.transform.position.y + height, player.transform.position.z - distance);
+        transform.LookAt(player.transform);
+    }
+}
---
+++ OculusRift/New Unity Project/Assets/Shoot_SpaceBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot_SpaceBar : MonoBehaviour {
+
+    public GameObject Emitter;
+    public GameObject Beam;
+    public float force;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            GameObject temp_beam;
+            temp_beam = Instantiate(Beam, Emitter.transform.position, Emitter.transform.rotation) as GameObject;
+
+            Rigidbody rb;
+            rb = temp_beam.GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * force);
+             
+            Destroy(temp_beam, 5.0f);
+        }
+	}
+}
